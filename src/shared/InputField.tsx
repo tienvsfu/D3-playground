3639397051692e96b585561b34723f947385113b
Tien Vu <tienvsfu@gmail.com>
2017-02-07 22:48:08 -0800

    Clean up InputField

--- src/shared/InputField.tsx
+++ src/shared/InputField.tsx
@@ -1,74 +1,21 @@
 import * as React from 'react';
-import { ControlLabel, Col, Row, Form, FormControl, FormGroup, Table } from 'react-bootstrap';
-
-import { d3Node } from '../types';
-import keyCodes from '../shared/keyCodes';
 
 interface Props {
   value?: string;
-  // onSave: Function;
-  // show: boolean;
   className?: string;
   style?: Object;
   onChange: Function;
   id: string;
   autoFocus?: boolean;
-  // keyHandler?: Function;
 }
 
 class InputField extends React.Component<Props, any> {
   private editInput;
 
-  constructor(props) {
-    super(props);
-
-    // this.state = {
-    //   value: props.value
-    // };
-  }
-
-  onBlur(e) {
-    // this.props.onSave({
-    //   name: this.state.value
-    // });
-
-    // e.stopPropagation();
-  }
-
   onChange(e) {
     const value = e.target.value;
-    // this.setState({
-    //   value
-    // });
-
     this.props.onChange(this.props.id, value);
   }
-
-  // onKeyDown(e) {
-  //   if (e.which === keyCodes.ENTER) {
-  //     this.props.onSave({
-  //       name: this.state.value
-  //     });
-
-  //     e.stopPropagation();
-  //   }
-
-  //   if (this.props.keyHandler) {
-  //     this.props.keyHandler(e);
-  //   }
-  // }
-
-  // componentDidMount() {
-  //   console.log('this shit mounted!');
-  // }
-
-  // componentWillReceiveProps(nextProps) {
-  //   if (nextProps.value) {
-  //     this.setState({
-  //       value: nextProps.value
-  //     });
-  //   }
-  // }
 
   componentDidUpdate() {
     if (this.editInput && this.props.autoFocus) {
@@ -78,13 +25,6 @@ class InputField extends React.Component<Props, any> {
 
   render() {
     let InputField = <input type="text" value={this.props.value} className={this.props.className} id={this.props.id} onChange={this.onChange.bind(this)} style={this.props.style} ref={(input) => this.editInput = input} />;
-
-    // if (this.props.show) {
-      // InputField = <input type="text" className={this.props.className} value={this.state.value} style={this.props.style} ref={(input) => this.editInput = input}
-      //         onChange={this.onChange.bind(this)}
-      //         onKeyDown={this.onKeyDown.bind(this)}
-      //         onBlur={this.onBlur.bind(this)} />;
-    // }
 
     return InputField;
   }
